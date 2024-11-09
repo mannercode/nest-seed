c94a2881c0afbb4c2ad534177847f5d30e67f36c
hunjuly <hunjuly@gmail.com>
2024-11-09 23:54:31 +0000

    .

--- jest.setup.ts
+++ jest.setup.ts
@@ -6,4 +6,3 @@ dotenv.config({ path: ['.env.app', '.env.infra'] })
 process.env.NODE_ENV = 'test'
 // testcontainers에서 testcontainers/ryuk 이미지 사용하지 않게 한다
 process.env.TESTCONTAINERS_RYUK_DISABLED = 'true'
-process.env.DOCKER_NETWORK = process.env.PROJECT_NAME
