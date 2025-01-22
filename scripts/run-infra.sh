84f8ae9b2195f9885a39d2d9b25f03d82db14888
hunjuly <hunjuly@gmail.com>
2025-01-22 22:54:44 +0000

    Add Kafka setup service to Docker Compose and update run-infra script

--- scripts/run-infra.sh
+++ scripts/run-infra.sh
@@ -5,7 +5,12 @@ set -e
 docker_compose --profile infra down --volumes --timeout 0
 docker_compose --profile infra up -d
 
-SETUP_CONTAINERS="${PROJECT_NAME}-mongo-key-generator ${PROJECT_NAME}-mongo-setup ${PROJECT_NAME}-redis-setup"
+SETUP_CONTAINERS="${PROJECT_NAME}-mongo-key-generator ${PROJECT_NAME}-mongo-setup ${PROJECT_NAME}-redis-setup ${PROJECT_NAME}-kafka-setup"
 
-docker wait $SETUP_CONTAINERS
-docker rm -v $SETUP_CONTAINERS
+for container in $SETUP_CONTAINERS; do
+    if [ "$(docker wait "$container")" -ne 0 ]; then
+        exit 1
+    else
+        docker rm -v "$container"
+    fi
+done
