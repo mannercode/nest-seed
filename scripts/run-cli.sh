2883c8b4fa9ffbac23f5a5c03eac6a79de50d9da
hunjuly <hunjuly@gmail.com>
2025-01-24 23:43:32 +0000

    Add Kafka support to CLI tool and update usage instructions

--- scripts/run-cli.sh
+++ scripts/run-cli.sh
@@ -7,7 +7,10 @@ if [ "$1" == "redis" ]; then
     docker exec -it "${REDIS_HOST1}" redis-cli -c -a $REDIS_PASSWORD
 elif [ "$1" == "mongo" ]; then
     docker exec -it "${MONGO_DB_HOST1}" mongosh -u "$MONGO_DB_USERNAME" -p "$MONGO_DB_PASSWORD" --authenticationDatabase admin
+elif [ "$1" == "kafka" ]; then
+    echo "--bootstrap-server ${KAFKA_BROKER1}:${KAFKA_PORT},${KAFKA_BROKER2}:${KAFKA_PORT},${KAFKA_BROKER3}:${KAFKA_PORT}"
+    docker exec --workdir /opt/kafka/bin/ -it $KAFKA_BROKER1 sh
 else
-    echo "Usage: $0 {redis|mongo}"
+    echo "Usage: $0 {redis|mongo|kafka}"
     exit 1
 fi
