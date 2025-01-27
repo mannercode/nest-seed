2088c853adaccef6c27288208bb31651423c3a80
hunjuly <hunjuly@gmail.com>
2025-01-27 05:47:40 +0000

    Add Kafka support with sample service and tests; update Docker Compose for restart policy

---
+++ src/libs/testlib/__tests__/create-testing-module.spec.ts
@@ -0,0 +1,34 @@
+import { TestingModule } from '@nestjs/testing'
+import { createTestingModule } from '..'
+import { SampleModule, SampleService } from './create-microservice-test-context.fixture'
+
+describe('createTestingModule', () => {
+    describe('overrideProviders', () => {
+        let module: TestingModule
+
+        beforeEach(async () => {
+            module = await createTestingModule({
+                imports: [SampleModule],
+                overrideProviders: [
+                    {
+                        original: SampleService,
+                        replacement: {
+                            getMessage: jest.fn().mockReturnValue({ message: 'This is Mock' })
+                        }
+                    }
+                ]
+            })
+        })
+
+        afterEach(async () => {
+            await module?.close()
+        })
+
+        it('overrideProviders에 설정한 mock 서비스가 응답해야 한다', async () => {
+            const service = module.get(SampleService)
+            const message = await service.getMessage('value')
+
+            expect(message).toEqual({ message: 'This is Mock' })
+        })
+    })
+})
