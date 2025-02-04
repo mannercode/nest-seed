e2020822e7f869d7929cf041be3951d3f777cee7
hunjuly <hunjuly@gmail.com>
2025-02-04 11:43:17 +0000

    Add NATS configuration to Docker Compose and update environment variables; modify run-infra.sh to include NATS setup

--- scripts/run-infra.sh
+++ scripts/run-infra.sh
@@ -5,14 +5,15 @@ set -e
 docker_compose --profile infra down --volumes --timeout 0
 docker_compose --profile infra up -d
 
-SETUP_CONTAINERS="${PROJECT_NAME}-mongo-key-generator ${PROJECT_NAME}-mongo-setup ${PROJECT_NAME}-redis-setup"
+SETUP_CONTAINERS="mongo-key-generator mongo-setup redis-setup nats-setup"
 
-for container in $SETUP_CONTAINERS; do
-    if [ "$(docker wait "$container")" -ne 0 ]; then
+for CONTAINER in $SETUP_CONTAINERS; do
+    CONTAINER_NAME="${PROJECT_NAME}-${CONTAINER}"
+
+    if [ "$(docker wait "$CONTAINER_NAME")" -ne 0 ]; then
+        echo "Error: Container '$CONTAINER_NAME' failed."
         exit 1
     else
-        docker rm -v "$container"
+        docker rm -v "$CONTAINER_NAME"
     fi
 done
-
-# bash $(dirname "$0")/kafka-create-topics.sh
