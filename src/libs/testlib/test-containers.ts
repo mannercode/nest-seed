98ffaef2d53627f146195c5d013c500ee918a3a3
hunjuly <hunjuly@gmail.com>
2025-08-07 23:43:35 +0000

    refactor tests

--- src/libs/testlib/test-containers.ts
+++ src/libs/testlib/test-containers.ts
@@ -26,7 +26,9 @@ export interface MongoConnectionContext {
 }
 
 export const getMongoTestConnection = (): MongoConnectionContext => {
-    const hosts = ['TEST_MONGO_HOST1', 'TEST_MONGO_HOST2', 'TEST_MONGO_HOST3'].map((key) => EnvVars.getString(key))
+    const hosts = ['TEST_MONGO_HOST1', 'TEST_MONGO_HOST2', 'TEST_MONGO_HOST3'].map((key) =>
+        EnvVars.getString(key)
+    )
     const port = EnvVars.getNumber('TEST_MONGO_PORT')
     const replicaName = EnvVars.getString('TEST_MONGO_REPLICA')
     const username = EnvVars.getString('TEST_MONGO_USERNAME')
@@ -42,7 +44,9 @@ export interface NatsConnectionContext {
 }
 
 export const getNatsTestConnection = (): NatsConnectionContext => {
-    const hosts = ['TEST_NATS_HOST1', 'TEST_NATS_HOST2', 'TEST_NATS_HOST3'].map((key) => EnvVars.getString(key))
+    const hosts = ['TEST_NATS_HOST1', 'TEST_NATS_HOST2', 'TEST_NATS_HOST3'].map((key) =>
+        EnvVars.getString(key)
+    )
     const port = EnvVars.getNumber('TEST_NATS_PORT')
     const servers = hosts.map((host) => `nats://${host}:${port}`)
 
