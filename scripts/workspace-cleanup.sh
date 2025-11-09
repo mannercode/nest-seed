6b3e4777d87cb5130eaa27658b3aa635acc6914c
hunjuly <hunjuly@gmail.com>
2025-11-09 03:46:51 +0000

    fix cleanup script

--- scripts/workspace-cleanup.sh
+++ scripts/workspace-cleanup.sh
@@ -2,8 +2,11 @@
 set -euo pipefail
 . "$(dirname "$0")/common.cfg"
 
-docker_compose --profile apps --profile infra down --rmi all
+docker rm -f $(docker ps -aq)
+docker system prune -af
+docker volume prune -af
+
+rm -rf $PROJECT_ROOT/node_modules
 
 # TODO log나 upload 같은 폴더 제거하면 sudo가 필요 없다.
 sudo rm -rf $PROJECT_ROOT/_output
-rm -rf $PROJECT_ROOT/node_modules
