be82276888c613ae81e547216bcf8e97c9f03f80
hunjuly <hunjuly@gmail.com>
2025-12-05 03:07:31 +0000

    refactor: jest scripts

--- tests/repeat/run.sh
+++ tests/repeat/run.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 . "$(dirname "$0")/utils.cfg"
 
-TEST_SUITES=("src" "apps" "common")
+TEST_SUITES=("src" "src/apps" "src/libs/common")
 
 if [ $# -ge 1 ]; then
 	TEST_SUITE="$1"
@@ -17,9 +17,11 @@ else
 	TEST_RUNS=${TEST_RUNS:-1}
 fi
 
+export TEST_ROOT=${TEST_SUITE}
+
 for ((i = 1; i <= TEST_RUNS; i++)); do
 	echo "[Run $i/$TEST_RUNS]"
-	npm run test:${TEST_SUITE}
+	npm test
 done
 
 echo "Done. $TEST_RUNS run(s) finished."
