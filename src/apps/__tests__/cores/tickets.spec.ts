6c1391d76f9363d2b1c424409c6ccb160e8706ff
hunjuly <hunjuly@gmail.com>
2025-12-14 11:23:11 +0000

    refactor: tickets

--- src/apps/__tests__/cores/tickets.spec.ts
+++ src/apps/__tests__/cores/tickets.spec.ts
@@ -17,93 +17,86 @@ describe('TicketsService', () => {
     })
 
     describe('createMany', () => {
-        describe('when the payload is valid', () => {
-            it('creates and returns tickets', async () => {
-                const createDtos = [buildCreateTicketDto({ sagaId: oid(0x1) })]
+        it('creates tickets', async () => {
+            const createDtos = [buildCreateTicketDto({ sagaId: oid(0x1) })]
 
-                const { success } = await fix.ticketsService.createMany(createDtos)
+            const { success } = await fix.ticketsService.createMany(createDtos)
 
-                expect(success).toBe(true)
-            })
+            expect(success).toBe(true)
         })
     })
 
     describe('search', () => {
-        const sagaId = oid(0x1)
-        const movieId = oid(0x2)
-        const theaterId = oid(0x3)
-        const showtimeId = oid(0x4)
-        let ticketForSaga: TicketDto
-        let ticketForMovie: TicketDto
-        let ticketForTheater: TicketDto
-        let ticketForShowtime: TicketDto
+        describe('when the filter is provided', () => {
+            const sagaId = oid(0x1)
+            const movieId = oid(0x2)
+            const theaterId = oid(0x3)
+            const showtimeId = oid(0x4)
+            let ticketForSaga: TicketDto
+            let ticketForMovie: TicketDto
+            let ticketForTheater: TicketDto
+            let ticketForShowtime: TicketDto
 
-        beforeEach(async () => {
-            ;[ticketForSaga] = await createTickets(fix, [{ sagaId }])
-            ;[ticketForMovie] = await createTickets(fix, [{ movieId }])
-            ;[ticketForTheater] = await createTickets(fix, [{ theaterId }])
-            ;[ticketForShowtime] = await createTickets(fix, [{ showtimeId }])
-        })
+            beforeEach(async () => {
+                ;[ticketForSaga, ticketForMovie, ticketForTheater, ticketForShowtime] =
+                    await createTickets(fix, [
+                        { sagaId },
+                        { movieId },
+                        { theaterId },
+                        { showtimeId }
+                    ])
+            })
 
-        describe('when the `sagaIds` are provided', () => {
-            it('returns tickets for the sagaIds', async () => {
+            it('returns tickets filtered by sagaIds', async () => {
                 const tickets = await fix.ticketsService.search({ sagaIds: [sagaId] })
 
                 expect(tickets).toEqual([ticketForSaga])
             })
-        })
 
-        describe('when the `movieIds` are provided', () => {
-            it('returns tickets for the movieIds', async () => {
+            it('returns tickets filtered by movieIds', async () => {
                 const tickets = await fix.ticketsService.search({ movieIds: [movieId] })
 
                 expect(tickets).toEqual([ticketForMovie])
             })
-        })
 
-        describe('when the `theaterIds` are provided', () => {
-            it('returns tickets for the theaterIds', async () => {
+            it('returns tickets filtered by theaterIds', async () => {
                 const tickets = await fix.ticketsService.search({ theaterIds: [theaterId] })
 
                 expect(tickets).toEqual([ticketForTheater])
             })
-        })
 
-        describe('when the `showtimeIds` are provided', () => {
-            it('returns tickets for the showtimeIds', async () => {
+            it('returns tickets filtered by showtimeIds', async () => {
                 const tickets = await fix.ticketsService.search({ showtimeIds: [showtimeId] })
 
                 expect(tickets).toEqual([ticketForShowtime])
             })
         })
 
-        describe('when the filter is empty', () => {
-            it('throws 400 status', async () => {
-                const promise = fix.ticketsService.search({})
+        it('throws 400 Bad Request for an empty filter', async () => {
+            const promise = fix.ticketsService.search({})
 
-                await expect(promise).rejects.toMatchObject({
-                    status: 400,
-                    message: Errors.Mongoose.FiltersRequired.message
-                })
+            await expect(promise).rejects.toMatchObject({
+                status: 400,
+                message: Errors.Mongoose.FiltersRequired.message
             })
         })
     })
 
     describe('updateStatusMany', () => {
-        let createdTickets: TicketDto[]
-
-        beforeEach(async () => {
-            createdTickets = await createTickets(fix, [
-                { status: TicketStatus.Available },
-                { status: TicketStatus.Available },
-                { status: TicketStatus.Available }
-            ])
-        })
-
         describe('when the tickets exist', () => {
-            it('updates ticket status and returns the tickets', async () => {
+            let tickets: TicketDto[]
+
+            beforeEach(async () => {
+                tickets = await createTickets(fix, [
+                    { status: TicketStatus.Available },
+                    { status: TicketStatus.Available },
+                    { status: TicketStatus.Available }
+                ])
+            })
+
+            it('returns the updated tickets', async () => {
                 const updatedTickets = await fix.ticketsService.updateStatusMany(
-                    pickIds(createdTickets),
+                    pickIds(tickets),
                     TicketStatus.Sold
                 )
 
@@ -115,18 +108,16 @@ describe('TicketsService', () => {
     })
 
     describe('aggregateSales', () => {
-        describe('when the `showtimeIds` are provided', () => {
+        describe('when the showtimeIds are provided', () => {
             const showtimeId = oid(0x10)
             const totalCount = 50
             const soldCount = 5
 
             beforeEach(async () => {
                 const createDtos = Array.from({ length: totalCount }, () => ({ showtimeId }))
-
                 const createdTickets = await createTickets(fix, createDtos)
 
                 const soldTickets = createdTickets.slice(0, soldCount)
-
                 await fix.ticketsService.updateStatusMany(pickIds(soldTickets), TicketStatus.Sold)
             })
 
