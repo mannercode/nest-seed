3d04586aef1ccccea89706257ab14de818d78e20
hunjuly <hunjuly@gmail.com>
2025-12-21 11:32:24 +0000

    update script

--- tests/repeat/run.sh
+++ tests/repeat/run.sh
@@ -2,22 +2,22 @@
 set -euo pipefail
 . "$(dirname "$0")/utils.cfg"
 
-TEST_SUITES=("src" "src/apps" "src/libs/common")
+TEST_ROOTS=("src" "src/apps" "src/libs/common")
 
 if [ $# -ge 1 ]; then
-	TEST_SUITE="$1"
+	TEST_ROOT="$1"
 	TEST_RUNS="${2:-1}"
 else
 	echo
 	echo "Select test suite:"
 
-	TEST_SUITE=$(prompt_selection "${TEST_SUITES[@]}")
+	TEST_ROOT=$(prompt_selection "${TEST_ROOTS[@]}")
 
 	read -p "Enter number of runs (default 500): " TEST_RUNS
 	TEST_RUNS=${TEST_RUNS:-500}
 fi
 
-export TEST_ROOT=${TEST_SUITE}
+export TEST_ROOT=${TEST_ROOT}
 
 for ((i = 1; i <= TEST_RUNS; i++)); do
 	echo "[Run $i/$TEST_RUNS]"
