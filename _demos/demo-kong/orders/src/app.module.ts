e909abcea7b50bc6e28568ac6c8f88ed52d7ad39
hunjuly <hunjuly@gmail.com>
2026-05-03 16:44:30 +0000

    chore: remove demo code

--- _demos/demo-kong/orders/src/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common'
-import { OrdersController } from './orders.controller'
-
-@Module({
-    controllers: [OrdersController]
-})
-export class AppModule {}
--- _demos/demo-kong/products/src/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common'
-import { ProductsController } from './products.controller'
-
-@Module({
-    controllers: [ProductsController]
-})
-export class AppModule {}
