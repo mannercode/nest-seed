03f76353e266229670d07b62a077701d68feb7ee
hunjuly <hunjuly@gmail.com>
2026-08-28 00:26:16 +0000

    ci: remove unused repeat reset branch

--- .github/scripts/repeat.sh
+++ .github/scripts/repeat.sh
@@ -116,16 +116,7 @@ trap on_failure ERR
 
 start_ts=$(date +%s)
 
-# 단위 테스트·부팅·race 시나리오가 함께 쓰는 반복 도구라 기본적으로 `RESET_EVERY` 회차마다 인프라를 초기화한다.
-# 사가 시나리오는 완료 기록과 앱 데이터가 누적되면 처리 지연이 커지므로, 회차당 워크플로 수가 많을수록 더 작은 값을 넘긴다.
-RESET_EVERY="${RESET_EVERY:-10}"
-RESET_SCRIPT="${WORKSPACE_ROOT:?WORKSPACE_ROOT must be set}/infra/reset.sh"
-
 for ((i = 1; i <= repeat_count; i++)); do
     echo "[Run ${i}/${repeat_count} | $(($(date +%s) - start_ts))s]"
-    if [ "${i}" -gt 1 ] && [ $(((i - 1) % RESET_EVERY)) -eq 0 ]; then
-        echo "[reset infra @ iter ${i}]"
-        bash "${RESET_SCRIPT}"
-    fi
     "$@"
 done
