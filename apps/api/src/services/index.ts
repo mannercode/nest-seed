503c26019c2fa0e97dd9f613815b59f958ef2020
hunjuly <hunjuly@gmail.com>
2026-08-30 16:44:11 +0000

    refactor(api): remove unused services barrel

--- apps/api/src/services/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './application/index.js'
-export * from './core/index.js'
-export * from './gateway/index.js'
-export * from './infrastructure/index.js'
-export * from './view/index.js'
